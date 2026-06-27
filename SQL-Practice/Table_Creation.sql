db5e714b4cad32385483624796bf054eebf42766
Abhishek Angadi <139436103+abhiangadi@users.noreply.github.com>
2026-06-27 12:54:30 +0530

    Update Table_Creation.sql

--- SQL-Practice/Table_Creation.sql
+++ SQL-Practice/Table_Creation.sql
@@ -1,1 +1,22 @@
+CREATE TABLE Student (
+    Rollno INT PRIMARY KEY,
+    Name VARCHAR(50),
+    Marks INT NOT NULL,
+    Grade VARCHAR(1),
+    City VARCHAR(20)
+);
 
+
+CREATE TABLE Dept (
+  id int primary key,
+  name varchar(50)
+);
+
+CREATE TABLE Teachers (
+id int primary key,
+name varchar(50),
+Dept_id int,
+Foreign key (Dept_id) references Dept(id)
+On Update cascade
+On Delete cascade
+);
