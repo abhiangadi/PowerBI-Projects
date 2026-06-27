f19aaf1cefded22cc7b430b8afe3d0af00bc7b6a
Abhishek Angadi <139436103+abhiangadi@users.noreply.github.com>
2026-06-27 13:01:20 +0530

    Update Aggregate_Functions.sql

--- SQL-Practice/Aggregate_Functions.sql
+++ SQL-Practice/Aggregate_Functions.sql
@@ -1,5 +1,31 @@
-SUM()
-AVG()
-MIN()
-MAX()
-COUNT()
+-- Total Revenue
+
+SELECT
+ROUND(SUM(quantity*price),2) AS Revenue
+FROM order_details od
+JOIN pizzas p
+ON od.pizza_id=p.pizza_id;
+
+-------------------------------------------------
+
+-- Average Pizza Price
+
+SELECT
+ROUND(AVG(price),2)
+FROM pizzas;
+
+-------------------------------------------------
+
+-- Highest Pizza Price
+
+SELECT
+MAX(price)
+FROM pizzas;
+
+-------------------------------------------------
+
+-- Lowest Pizza Price
+
+SELECT
+MIN(price)
+FROM pizzas;
