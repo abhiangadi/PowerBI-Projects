4ae31d2f185d024e631bec975e44dc547480469b
Abhishek Angadi <139436103+abhiangadi@users.noreply.github.com>
2026-06-27 12:51:50 +0530

    Create Basic_Queries.sql

---
+++ SQL-Practice/Basic_Queries.sql
@@ -0,0 +1,15 @@
+-- View all orders
+SELECT * FROM orders;
+
+-- View all pizzas
+SELECT * FROM pizzas;
+
+-- View pizza types
+SELECT * FROM pizza_types;
+
+-- View order details
+SELECT * FROM order_details;
+
+-- Total Orders
+SELECT COUNT(*) AS Total_Orders
+FROM orders;
